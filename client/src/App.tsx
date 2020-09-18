bac78a0081f3b8fb1f9fea959fe89d04b72bee2e
Andy <andrewcuccinello@gmail.com>
2020-09-18 14:03:24 -0400

    Setting tab with back after testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,26 +3,25 @@ import logo from './onion.png';
 import './App.css';
 
 function App() {
-       return (
-              <div className="App">
-                     <header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" />
-                            <p>Cooking with Elvin (and React)</p>
-                            <p>
-                                   Edit <code>src/App.tsx</code> and save to
-                                   reload.
-                            </p>
-                            <a
-                                   className="App-link"
-                                   href="https://reactjs.org"
-                                   target="_blank"
-                                   rel="noopener noreferrer"
-                            >
-                                   Learn React
-                            </a>
-                     </header>
-              </div>
-       );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Cooking with Elvin (and React)</p>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
