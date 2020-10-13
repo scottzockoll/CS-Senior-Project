be5f3835f823e727b959b26aa2c99b3b03aa6cb9
Andy <andrewcuccinello@gmail.com>
2020-10-13 10:18:52 -0400

    Add output argument

--- server/__main__.py
+++ server/__main__.py
@@ -1,4 +1,5 @@
 from server import app
+import os
 from server.model.train import train_model
 import argparse
 
@@ -6,13 +7,18 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='FlickPick server.')
     parser.add_argument('--train', action='store_true', help="Start the server in model training mode.")
-
-
+    parser.add_argument('-o', '--output', type=str, help='Name of output model to be saved.')
 
     args = parser.parse_args()
 
     if args.train:
-        train_model()
+        if not args.output:
+            raise ValueError('An output file must be specified in training mode.')
+
+        train_model(
+            output_folder=os.path.abspath('model'),
+            output_name=args.output
+        )
 
 
 if __name__ == '__main__':
