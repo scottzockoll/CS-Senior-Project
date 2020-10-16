a6d7f8c5d85cb72f5a48d517f22980f054451e9b
thefinancialfix <thefinancialfix@users.noreply.github.com>
2020-10-16 15:26:17 -0400

    code review - Andy pt 2

--- client/src/routes/common/NavigationBar.tsx
+++ client/src/routes/common/NavigationBar.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Button, Grommet, Header, Menu, Image } from 'grommet';
+import { Button, Grommet, Header, Menu, Image, Box } from 'grommet';
 
 function NavigationBar() {
     return (
-        <div>
-            <Grommet className="NavigationBar">
-                <Header style={{ height: 60 }} background="brand">
-                    <Button plain={true} label="FlickPick" hoverIndicator />
-                    <Menu
-                        dropBackground="white"
-                        label="Sign Up"
-                        items={[{ label: 'Sign In' }, { label: 'Home' }]}
-                    />
-                </Header>
-            </Grommet>
-        </div>
+        <Box>
+            <Header style={{ height: 60 }} background="brand">
+                <Button plain={true} label="FlickPick" hoverIndicator />
+                <Menu
+                    dropBackground="white"
+                    label="Sign Up"
+                    items={[{ label: 'Sign In' }, { label: 'Home' }]}
+                />
+            </Header>
+        </Box>
     );
 }
 
--- client/src/routes/home/Homepage.tsx
+++ client/src/routes/home/Homepage.tsx
@@ -1,34 +1,20 @@
 import { Box, Carousel, Heading, Image } from 'grommet';
 import React from 'react';
-import './Homepage.css';
 
 function Homepage() {
     return (
-        <div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <Heading textAlign="center" margin="none">
-                Welcome to FlickPick!{' '}
-            </Heading>
-            <br></br>
-            <br></br>
-            <Box
-                alignContent="center"
-                height="medium"
-                width="large"
-                overflow="visible"
-            >
+        <Box background="light-3">
+            <Box justify="center" direction="row">
+                <Heading margin="large">Welcome to FlickPick! </Heading>
+            </Box>
+            <Box justify="center" direction="row">
                 <Carousel play={5000}>
                     <Image src="images/movie1.jpg" />
                     <Image src="images/movie2.jpg" />
                     <Image src="images/movie3.jpg" />
                 </Carousel>
             </Box>
-        </div>
+        </Box>
     );
 }
 
