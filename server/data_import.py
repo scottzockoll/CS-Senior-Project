ad7fc648bc9729a39d0bcc7818eb684f69d78db8
rlevari <levari82@students.rowan.edu>
2020-10-16 15:23:51 -0400

    Adjust DB connection, imported data from dataset
    - Changed DB connection from using flask_sqlalchemy/sqlalchemy to mysql connector
    - read in data from csv's using pandas and created data frames
    - using data frames, I pulled out the necessary data needed per table and created a tuple list per table
    - for user data I created randomized first/last name combinations using csv's found online
    - mysql connector has a batch execute for queries, each query will execute through each tuple in the tuple list per table

--- server/data_import.py
+++ server/data_import.py
@@ -1,28 +1,29 @@
-import mysql.connector
 import pandas as pd
 import time
 from mysql.connector import connection
-import csv
 import random
 
 start_time = time.time()
 
+# This will change depending on dataset being used
+core_folder = '../dataset/ml-latest-small/'
+
 # DataFrames from CSV's
-movies = pd.read_csv(r'../dataset/ml-latest-small/movies.csv')
+movies = pd.read_csv(core_folder + 'movies.csv')
 movie_df = pd.DataFrame(movies, columns=['movieId', 'title', 'genres'])
 
-ratings = pd.read_csv(r'../dataset/ml-latest-small/ratings.csv')
-rating_df = pd.DataFrame(ratings, columns=['userId', 'movieId', 'rating'])
+ratings = pd.read_csv(core_folder + 'ratings.csv')
+rating_df = pd.DataFrame(ratings, columns=['userId', 'movieId', 'rating', 'timestamp'])
 
-tags = pd.read_csv(r'../dataset/ml-latest-small/tags.csv')
+tags = pd.read_csv(core_folder + 'tags.csv')
 tag_df = pd.DataFrame(tags, columns=['userId', 'movieId', 'tag'])
 
 # Fake User Data
-fName = pd.read_csv(r'../dataset/user_data/baby-names.csv')
+fName = pd.read_csv('../dataset/user_data/baby-names.csv')
 fName_df = pd.DataFrame(fName, columns=['name'])
 
 # Fake User Data
-lName = pd.read_csv(r'../dataset/user_data/surnames.csv')
+lName = pd.read_csv('../dataset/user_data/surnames.csv')
 lName_df = pd.DataFrame(lName, columns=['name'])
 
 # Tuple lists per table
@@ -30,7 +31,7 @@ movieTable = []
 genreTable = []
 userTable = []
 tagsTable = []
-#tag_feedbackTable = []
+tag_feedbackTable = []
 movie_feedbackTable = []
 
 # Movie ID and Title from movies.csv
@@ -46,19 +47,18 @@ for g_row in movie_df.itertuples():
         genreTable.append(gt)
 
 # User ID, Movie ID and Rating from ratings.csv
-for mf_row in rating_df.itertuples():
+for mf_row in rating_df.head().itertuples():
     mft = [mf_row.rating, mf_row.movieId, mf_row.userId]
     movie_feedbackTable.append(mft)
 
-# Sorting movieId column for convenience, then comparing between movies.csv and tags.csv to make sure a movie is in both
-#csv_sort = tags.sort_values(by=['movieId'])
-#test = csv_sort['movieId'].isin(movies['movieId'])
-#t = csv_sort[test]
-
-# Movie ID from movies.csv, Tag Name from tags.csv
+# Tag name, Movie ID and User ID from tags.csv
+tagID = 1
 for t_row in tag_df.itertuples():
     tt = [t_row.tag, t_row.movieId]
+    tft = [5, t_row.movieId, t_row.userId, tagID]
+    tagID += 1
     tagsTable.append(tt)
+    tag_feedbackTable.append(tft)
 
 # Fake User Data Creation
 # Converts baby-names.csv and surnames.csv DataFrames to Lists
@@ -69,8 +69,8 @@ last = lName_df['name'].tolist()
 for i in range(1, 611):  # 610 entries since ratings.csv has 610 different userID's (this is only for fake data)
     first_name = str(random.choice(first))
     last_name = str(random.choice(last)).lower().title()
-    email = first_name.lower() + last_name.lower() + "@s.r.e"
-    ut = [first_name, last_name, email, 1]
+    email = first_name.lower() + last_name.lower() + "@s.r.e"   # s.r.e = students.rowan.edu
+    ut = [first_name, last_name, email, 1]                      # everyone is made an admin for time being
     userTable.append(ut)
 
 # Connection to Database
@@ -78,18 +78,16 @@ d_con = connection.MySQLConnection(user='fp_user', password='flickpick123',
                                    host='ec2-18-222-97-98.us-east-2.compute.amazonaws.com', database='FlickPick')
 cursor = d_con.cursor()
 
-# Batch query executions per table using tuple lists
-#cursor.executemany("INSERT INTO movies (id, name) VALUES (%s, %s)", movieTable)
-#cursor.executemany("INSERT INTO genre (movie_id, genre) VALUES (%s, %s)", genreTable)
-#cursor.executemany("INSERT INTO users (firstName, lastName, email, isAdmin) VALUES (%s, %s, %s, %s)", userTable)
-#cursor.executemany("INSERT INTO tags (name, movie_id) VALUES (%s, %s)", tagsTable)
+# Batch query executions per table using tuple lists (ONLY RUN IF TABLES ARE EMPTY AND AUTO_INCREMENT IS SET BACK TO 1)
+cursor.executemany("INSERT INTO movies (id, name) VALUES (%s, %s)", movieTable)
+cursor.executemany("INSERT INTO genre (movie_id, genre) VALUES (%s, %s)", genreTable)
+cursor.executemany("INSERT INTO users (firstName, lastName, email, isAdmin) VALUES (%s, %s, %s, %s)", userTable)
 cursor.executemany("INSERT INTO movie_feedback (rating, movie_id, user_id) VALUES (%s, %s, %s)", movie_feedbackTable)
-#cursor.execute("INSERT INTO tag_feedback (rating, movie_id, user_id, tag_id) "
-#               "SELECT 5, movie_feedback.movie_id, movie_feedback.user_id, tags.id "
-#               "FROM movie_feedback, tags WHERE movie_feedback.movie_id=tags.movie_id"
-#               "AND movie_feedback.user_id=tags.user_id")
+cursor.executemany("INSERT INTO tags (name, movie_id) VALUES (%s, %s)", tagsTable)
+cursor.executemany("INSERT INTO tag_feedback (rating, movie_id, user_id, tag_id) VALUES (%s, %s, %s, %s)", tag_feedbackTable)
 
 d_con.commit()
+
 cursor.close()
 d_con.close()
 
