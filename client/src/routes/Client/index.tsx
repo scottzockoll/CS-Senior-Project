3078d3206ce511e4da2776035ac9dec828979357
Jasonz923 <66922268+jasonz923@users.noreply.github.com>
2020-10-23 08:42:09 -0400

    Update index.tsx

--- client/src/routes/Client/index.tsx
+++ client/src/routes/Client/index.tsx
@@ -1,58 +1,40 @@
+
 import React from 'react';
-import { Box, DataTable, Text, Button } from 'grommet';
-import { UserRecord } from '../admin/UserRecord';
+import { Box, DataTable, Text, Button, Grommet} from 'grommet';
+import { UserRecord, WatchedMovie } from '../admin/UserRecord';
+import NavigationBar from '../common/NavigationBar';
 
-/*
-let movieRecords: WatchedMovie[] = [
-  {
-    title: "Avengers Endgame (2019)",
-    rating: 5,
-  },
-  {
-    title: "Goon (2011)",
-    rating: 3,
-  },
-  {
-    title: "Justice League (2017)",
-    rating: 3.5,
-  },
-];
 
-const userRecord: UserRecord =
-  {
-    firstName: "Jason",
-    lastName: "Zogheb",
-    email: "jz@fakeemail.com",
-    watchedMovies: movieRecords,
-  };
-*/
 
 interface ClientPageProps {
     userRecord: UserRecord;
 }
 
-// function resetMovies(userRecord: UserRecord){
-//   userRecord.watchedMovies = []
-//   ClientPage(userRecord)
-//
-// }
 
 export default function ClientPage(props: ClientPageProps) {
-    return (
-        <Box align="center" pad="large">
-            <Text>
-                User: {props.userRecord.firstName} {props.userRecord.lastName}
-            </Text>
-            <Text>Email: {props.userRecord.email}</Text>
-            <DataTable
-                border={true}
-                columns={[
-                    { property: 'title', header: 'Rating Survey(s)' },
-                    { property: 'userRating', header: 'Rating' },
-                ]}
-                data={props.userRecord.watchedMovies}
-            />
-            <Button label="Reset Movie Survey" />
-        </Box>
-    );
+  return (
+    <Grommet full={true} >
+      <NavigationBar />
+      <Box pad="medium" align="start">
+        <Text>User: {props.userRecord.firstName} {props.userRecord.lastName}</Text>
+        <Text>Email: {props.userRecord.email}</Text>
+      </Box>
+      <Box width="large" pad="medium">
+        <DataTable border={true}
+          columns={[
+            { property: 'title', header: 'Rating Survey(s)' },
+            { property: 'userRating', header: 'Rating' },
+          ]}
+          data={props.userRecord.watchedMovies}
+        />
+      </Box>
+      <Box pad="medium">
+        <Button label="Reset Movie Survey" fill = {false} alignSelf="start" />
+      </Box>
+      <Box as="footer" direction="row" justify="end">
+        <Button label="Sign Out" />
+        <Button label="Delete Account" />
+      </Box>
+    </Grommet>
+  );
 }
