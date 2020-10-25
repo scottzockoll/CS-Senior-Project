55986d83e8464b634292ccfd79883c957aa3368f
Jasonz923 <66922268+jasonz923@users.noreply.github.com>
2020-10-25 18:50:05 -0400

    resolved en.json changes
    resolved en.json changes

--- client/src/routes/Client/index.tsx
+++ client/src/routes/Client/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, DataTable, Text, Button, Layer, Grommet } from 'grommet';
 import { UserRecord, WatchedMovie } from '../admin/UserRecord';
 import en from '../../en.json';
+
 interface ClientPageProps {
     userRecord: UserRecord;
 }
@@ -12,74 +13,85 @@ export default function ClientPage(props: ClientPageProps) {
     const [showResetMovies, setShowResetMovies] = React.useState<boolean>();
 
     return (
-        <Grommet full={true}>
-            <Box pad="medium" align="start">
-                <Text>
-                    User: {props.userRecord.firstName} {props.userRecord.lastName}
-                </Text>
-                <Text>Email: {props.userRecord.email}</Text>
-            </Box>
-            <Box width="large" pad="medium">
-                <DataTable
-                    border={true}
-                    columns={[
-                        { property: 'title', header: en.UI_LABELS.title },
-                        { property: 'userRating', header: en.UI_LABELS.userRating },
-                    ]}
-                    data={props.userRecord.watchedMovies}
-                />
-            </Box>
-            <Box pad="medium">
-                <Button
-                    label={en.UI_LABELS.resetMovieSurvey}
-                    fill={false}
-                    alignSelf="start"
-                    onClick={() => setShowResetMovies(true)}
-                />
-                {showResetMovies && (
-                    <Layer onEsc={() => setShowResetMovies(false)} onClickOutside={() => setShowResetMovies(false)}>
-                        <Box justify="center">
-                            <Text size="xxlarge">Reset Your Movies?</Text>
-                            <Text>----------------------------------------</Text>
-                        </Box>
+            <Grommet>
+                <Box pad="medium" align="start">
+                    <Text>
+                        {en.UI_LABELS.fullName}: {props.userRecord.firstName} {props.userRecord.lastName}
+                    </Text>
+                    <Text>
+                        {en.UI_LABELS.email}: {props.userRecord.email}
+                    </Text>
+                </Box>
+                <Box width="large" pad="medium">
+                    <DataTable
+                        border={true}
+                        columns={[
+                            { property: 'title', header: en.UI_LABELS.title },
+                            { property: 'userRating', header: en.UI_LABELS.userRating },
+                        ]}
+                        data={props.userRecord.watchedMovies}
+                    />
+                </Box>
+                <Box>
+                    <Button
+                        label={en.UI_LABELS.resetMovieSurvey}
+                        fill={false}
+                        alignSelf="start"
+                        onClick={() => setShowResetMovies(true)}
+                    />
+                    {showResetMovies && (
+                        <Layer onEsc={() => setShowResetMovies(false)} onClickOutside={() => setShowResetMovies(false)}>
+                            <Box justify="center">
+                                <Text size="xxlarge">{en.UI_LABELS.resetYourMovies}</Text>
+                                <Text>----------------------------------------</Text>
+                            </Box>
 
-                        <Box direction="row" justify="center">
-                            <Button label="Yes" onClick={() => setShowResetMovies(false)} />
-                            <Button label="No" onClick={() => setShowResetMovies(false)} />
-                        </Box>
-                    </Layer>
-                )}
-            </Box>
+                            <Box direction="row" justify="center">
+                                <Button label={en.UI_LABELS.yes} onClick={() => setShowResetMovies(false)} />
+                                <Button label={en.UI_LABELS.no} onClick={() => setShowResetMovies(false)} />
+                            </Box>
+                        </Layer>
+                    )}
+                </Box>
+                <Box direction="row" alignSelf="end" justify="end" >
+                    <Button
+                        label={en.UI_LABELS.signOut}
+                        fill={false}
+                        alignSelf="end"
+                        onClick={() => setShowSignOut(true)}
+                    />
+                    {showSignOut && (
+                        <Layer onEsc={() => setShowSignOut(false)} onClickOutside={() => setShowSignOut(false)}>
+                            <Box justify="center">
+                                <Text size="xxlarge">{en.UI_LABELS.signOutQuestion}</Text>
+                                <Text>---------------------</Text>
+                            </Box>
+                            <Box direction="row" justify="center">
+                                <Button label={en.UI_LABELS.yes} onClick={() => setShowSignOut(false)} />
+                                <Button label={en.UI_LABELS.no} onClick={() => setShowSignOut(false)} />
+                            </Box>
+                        </Layer>
+                    )}
 
-            <Box as="footer" direction="row" justify="end">
-                <Button label={en.UI_LABELS.signOut} onClick={() => setShowSignOut(true)} />
-                {showSignOut && (
-                    <Layer onEsc={() => setShowSignOut(false)} onClickOutside={() => setShowSignOut(false)}>
-                        <Box justify="center">
-                            <Text size="xxlarge">Sign Out?</Text>
-                            <Text>---------------------</Text>
-                        </Box>
-                        <Box direction="row" justify="center">
-                            <Button label="Yes" onClick={() => setShowSignOut(false)} />
-                            <Button label="No" onClick={() => setShowSignOut(false)} />
-                        </Box>
-                    </Layer>
-                )}
-
-                <Button label={en.UI_LABELS.deleteAccount} onClick={() => setShowDeleteAccount(true)} />
-                {showDeleteAccount && (
-                    <Layer onEsc={() => setShowDeleteAccount(false)} onClickOutside={() => setShowDeleteAccount(false)}>
-                        <Box justify="center">
-                            <Text size="xxlarge">Delete Your Account?</Text>
-                            <Text>--------------------------------------------</Text>
-                        </Box>
-                        <Box direction="row" justify="center">
-                            <Button label="Yes" onClick={() => setShowDeleteAccount(false)} />
-                            <Button label="No" onClick={() => setShowDeleteAccount(false)} />
-                        </Box>
-                    </Layer>
-                )}
-            </Box>
-        </Grommet>
+                    <Button
+                        label={en.UI_LABELS.deleteAccount}
+                        fill={false}
+                        alignSelf="end"
+                        onClick={() => setShowDeleteAccount(true)}
+                    />
+                    {showDeleteAccount && (
+                        <Layer onEsc={() => setShowDeleteAccount(false)} onClickOutside={() => setShowDeleteAccount(false)}>
+                            <Box justify="center">
+                                <Text size="xxlarge">{en.UI_LABELS.deleteYourAccount}</Text>
+                                <Text>--------------------------------------------</Text>
+                            </Box>
+                            <Box direction="row" justify="center">
+                                <Button label={en.UI_LABELS.yes} onClick={() => setShowDeleteAccount(false)} />
+                                <Button label={en.UI_LABELS.no} onClick={() => setShowDeleteAccount(false)} />
+                            </Box>
+                        </Layer>
+                    )}
+                </Box>
+            </Grommet>
     );
 }
