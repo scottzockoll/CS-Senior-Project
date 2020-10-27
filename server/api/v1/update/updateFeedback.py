4602c6e09b3657a08191f200bf7080a9288f0b9e
Justus Fee <59373880+justusRSU@users.noreply.github.com>
2020-10-27 10:12:17 -0400

    Added update_feedback
    A little buggy, but available for testing

---
+++ server/api/v1/update/updateFeedback.py
@@ -0,0 +1,38 @@
+from server.utilities import db_connection
+from flask import Response
+
+
+def update_feedback(feedbackId: int):
+    """
+    Replace a user's feedback of a specific movie
+    :param int feedbackId: The feedback id to retrieve
+    :return: Nothing
+    """
+    con, cursor = db_connection()
+    
+    # The line below is for the request body content, which is awaiting implementation on the frontend.
+    # rating = request.form["rating"]
+    
+    # For now it is hardcoded for testing purposes
+    rating = 1
+    
+    try:
+        # Validate user permission level (eventually will come from OAuth)
+        if not True:
+            return Response({}, mimetype='application/json', status=403)
+        
+        # Validate input parameters
+        if not isinstance(feedbackId, int):
+            return Response({}, mimetype='application/json', status=400)
+        
+        # Update row in database
+        cursor.execute("UPDATE movie_feedback SET rating={r} WHERE id={f}".format(r = rating, f = feedbackId))
+        if cursor.rowcount() < 1:
+            return Response({}, mimetype='application/json', status=404)
+        else:
+            return Response({}, mimetype='application/json', status=200)
+    except Exception:
+        return Response({}, mimetype='application/json', status=500)
+    finally:
+        cursor.close()
+        con.close()
