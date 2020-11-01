b0a50700fd8a35e720f751805c1fcbcd5b6cfe05
Andy <andrewcuccinello@gmail.com>
2020-10-31 20:27:52 -0400

    Move top-level components to top-level

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { applyMiddleware, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import loggerMiddleware from 'redux-logger';
+import { Provider } from 'react-redux';
+import { rootReducer } from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
