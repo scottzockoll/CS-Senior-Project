67997140a187b39f860c7dece71a251f4e773f9f
Reno Levari <55299294+rlevari@users.noreply.github.com>
2020-11-04 17:37:53 -0500

    API Endpoints & Data Import (#89)
    * Created function for database connection
    
    * Updated Database connection function based on Andy's input.
    
    * Fixed movie feedback import in data_import.py (removed .head())
    
    Created proper api directories under server
    
    * wrote functionality for get_tag
    
    * wrote functionality for del_user
    
    * Added update_feedback
    
    A little buggy, but available for testing
    
    * Fixed function to work
    
    Changed rowcount from function to attribute. Also added commit and rollback.
    
    * Another quick fix
    
    Hinting that rollback may be unnecessary due to the logic block I have
    
    * Created update_feedback_tag endpoint
    
    Works perfectly
    
    * updated del_user.py
    
    * forgot to delete my testing line on previous commit
    
    * Created endpoint for get_movie
    
    This endpoint functions to get a movie's id, title and associated tags when receiving a movie id.
    
    * Created createFeedback endpoint
    
    Works as intended, returns ID of created entry
    
    * Created createFeedbackTag endpoint
    
    Works as intended and returns ID of created entry
    
    * updated data_import.py to work with 25M dataset
    
    * Added TODOs for some comments
    
    * syntax changes, added TODOs
    
    Co-authored-by: Justus Fee <59373880+justusRSU@users.noreply.github.com>
    Co-authored-by: robinsk94 <60240812+robinsk94@users.noreply.github.com>

---
+++ server/api/v1/create/createFeedbackTag.py
@@ -0,0 +1,46 @@
+from server.utilities import db_connection
+from flask import Response
+import json
+
+
+def create_feedback_tag(userId: int, movieId: int, tagId: int):
+    """
+    Replace the feedback for a specific tag id
+    :param int userId: The user id to retrieve
+    :param int movieId: The movie id to retrieve
+    :param int tagId: The tag id to retrieve
+    :return: JSON object of feedback id
+    """
+    con, cursor = db_connection()
+    
+    # The line below is for the request body content, which is awaiting implementation on the frontend.
+    # rating = request.form["rating"]
+    
+    # TODO: For now it is hardcoded for testing purposes
+    rating = 1
+    
+    try:
+        # TODO: Validate user permission level (eventually will come from OAuth)
+        if not True:
+            return Response({}, mimetype='application/json', status=403)
+        
+        # Validate input parameters
+        if not isinstance(userId, int) and not isinstance(movieId, int) and not isinstance(tagId, int):
+            return Response({}, mimetype='application/json', status=400)
+        
+        # Create row in database
+        cursor.execute("INSERT INTO tag_feedback (rating, movie_id, user_id, tag_id) VALUES ({r}, {m}, {u}, {t})".format(r = rating, m = movieId, u = userId, t = tagId))
+        if cursor.rowcount == 1:
+            con.commit()
+            data = {
+                "id": cursor.lastrowid
+            }
+            return Response(json.dumps(data), mimetype='application/json', status=201)
+        else:
+            con.rollback()
+            return Response({}, mimetype='application/json', status=404)
+    except Exception:
+        return Response({}, mimetype='application/json', status=500)
+    finally:
+        cursor.close()
+        con.close()
