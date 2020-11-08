adacaef215fb7f510b23e82d02d97764d1c10352
robinsk94 <60240812+robinsk94@users.noreply.github.com>
2020-11-08 13:04:31 -0500

    Get user (#94)
    * Created function for database connection
    
    * Updated Database connection function based on Andy's input.
    
    * Fixed movie feedback import in data_import.py (removed .head())
    
    Created proper api directories under server
    
    * wrote functionality for get_tag
    
    * wrote functionality for del_user
    
    * Added update_feedback
    
    A little buggy, but available for testing
    
    * Fixed function to work
    
    Changed rowcount from function to attribute. Also added commit and rollback.
    
    * Another quick fix
    
    Hinting that rollback may be unnecessary due to the logic block I have
    
    * Created update_feedback_tag endpoint
    
    Works perfectly
    
    * updated del_user.py
    
    * forgot to delete my testing line on previous commit
    
    * Created endpoint for get_movie
    
    This endpoint functions to get a movie's id, title and associated tags when receiving a movie id.
    
    * Created createFeedback endpoint
    
    Works as intended, returns ID of created entry
    
    * Created createFeedbackTag endpoint
    
    Works as intended and returns ID of created entry
    
    * updated data_import.py to work with 25M dataset
    
    * Added TODOs for some comments
    
    * syntax changes, added TODOs
    
    * deleted placeholder endpoints in api.py, updated stubs
    
    * Created get_feedback endpoint
    
    Returns movie ID and rating
    
    * Created get_feedback_tags endpoint
    
    Returns array of feedback objects containing movie ID, tag ID, and tag rating
    
    * functionality to get user's info and rating info added
    
    * Pushed get_user.py
    
    Fixed the issue of get_user.py being missing from the pull request
    
    * Update get_movie.py
    
    Updated get_movie.py to generate less rows when it queries the database through the use of aggregation of tags and genres. get_movie.py also includes movie genres in the json object that is returned in the response.
    
    Co-authored-by: rlevari <levari82@students.rowan.edu>
    Co-authored-by: Justus Fee <59373880+justusRSU@users.noreply.github.com>

--- server/api/v1/get/get_movie.py
+++ server/api/v1/get/get_movie.py
@@ -18,10 +18,13 @@ def get_movie(id: int):
             return Response({
             }, mimetype='application/json', status=400)
         else:  # executes query
-            cursor.execute('''SELECT DISTINCT movies.id, movies.name, tags.name FROM FlickPick.movies '''
+            cursor.execute('''SELECT distinct movies.id, movies.name, group_concat(DISTINCT tags.name ORDER BY tags.name ASC SEPARATOR ',') as 'Tags', '''
+                           '''group_concat(DISTINCT genre.genre ORDER BY genre.genre ASC SEPARATOR ',') as 'Genres' FROM FlickPick.movies '''
                            '''JOIN tags '''
                            '''ON tags.movie_id = movies.id '''
-                           '''WHERE movies.id = 60756;'''(id, ))
+                           '''JOIN genre '''
+                           '''on genre.movie_id = movies.id '''
+                           '''WHERE movies.id = %s;''', (id, ))
             result = cursor.fetchall()
 
             if len(result) < 1:  # make sure there is at least one result returned
@@ -30,11 +33,13 @@ def get_movie(id: int):
             else:
                 movie_id = result[0][0]
                 movie_name = result[0][1]
-                tags = [genres[2] for genres in result]
+                tags = [tags[2].split(',') for tags in result]
+                genres = [genres[3].split(',') for genres in result]
                 data = {
                     "movie_id": movie_id,
                     "movie_name": movie_name,
-                    "tags": tags
+                    "tags": tags,
+                    "genres": genres
                 }
 
                 return Response(json.dumps(data), mimetype='application/json', status=200)
