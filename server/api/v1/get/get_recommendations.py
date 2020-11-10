7d33f0770ca50c82e183d9ba0bc782f590cd0317
Andy <andrewcuccinello@gmail.com>
2020-11-09 19:27:26 -0500

    Basic recommendation endpoint

--- server/api/v1/get/get_recommendations.py
+++ server/api/v1/get/get_recommendations.py
@@ -1,7 +1,7 @@
-import os
 import pickle
+import json
 
-from server.utilities import db_connection, is_admin, is_user
+from server.utilities import db_connection
 import numpy as np
 
 model_name = '100k'
@@ -11,22 +11,52 @@ def get_recommendations(user_id: int):
     # TODO: Check auth for this endpoint
     connection, cursor = db_connection()
 
+    limit = 10
+
     def magnitude(a: np.ndarray):
-        return np.sqrt(np.dot(a, a))
+        return np.linalg.norm(a)
 
     def similarity(a: np.ndarray, b: np.ndarray):
         return np.dot(a, b)/(magnitude(a)*magnitude(b))
 
     with open(f"model/mf/{model_name}_d.mat.pickle", 'rb') as file:
-        users = pickle.loads(file.read())
+        ratings_mat = pickle.loads(file.read())
 
-    matrix = np.load(f"model/mf/{model_name}_r.npy")
+    predict_mat = np.load(f"model/mf/{model_name}_r.npy")
 
-    n_movies = cursor.execute("SELECT COUNT(*) FROM movies;")
-    print(f"n_movies: {n_movies}")
-    raw_ratings = cursor.execute(f"SELECT * FROM movie_feedback WHERE user_id={user_id};")
-    print("raw_ratings")
-    print(raw_ratings)
-    # ratings = np.zeros(n_movies)
+    # TODO; Slow. Should be loaded from disk.
+    cursor.execute("SELECT MAX(movie_id) FROM movie_feedback;")
+    model_size = cursor.fetchone()[0]
 
+    cursor.execute(f"SELECT movie_id-1 FROM movie_feedback WHERE user_id={user_id};")
+    user_ratings = cursor.fetchall()
 
+    user_ratings = np.array(user_ratings).flat
+    user_ratings_dense = np.zeros(model_size)
+    np.put(user_ratings_dense, user_ratings, [1])
+    user_ratings = set(user_ratings)
+
+    # calculate which users are the most similar to the requested user
+    similarities = [(idx, similarity(user_ratings_dense, row.todense().flat)) for idx, row in enumerate(ratings_mat)]
+    similarities = sorted(similarities, key=lambda x: x[1], reverse=True)
+
+    # calculate the candidates based on the top 5 most similar users
+    # some movies will overlap, we'll weight those stronger
+    candidates = np.zeros_like(user_ratings_dense)
+    for sim_id, sim_val in similarities[:5]:
+        candidates += ratings_mat[sim_id - 1].todense().flat * predict_mat[sim_id - 1]
+
+    candidates = list(
+        map(
+            lambda x: x[0],
+            sorted(
+                filter(
+                    lambda x: x[0] not in user_ratings,
+                    enumerate(candidates)
+                ),
+                key=lambda x: x[1],
+                reverse=True
+            )
+        )
+    )
+    return json.dumps(candidates[:limit])
