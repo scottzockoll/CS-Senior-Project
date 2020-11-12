f42dc01b58a227d4fae58c865fd12ac259a84f95
rlevari <levari82@students.rowan.edu>
2020-11-12 18:39:08 -0500

    Helper functions created, TODOs completed, auth_user.py finished

--- server/api/v1/auth/auth_user.py
+++ server/api/v1/auth/auth_user.py
@@ -1,46 +1,15 @@
 import json
 
-from flask import session, request
-
-from server.utilities import db_connection
-from flask import Response
-from server.auth import authenticate
+from flask import session, request, Response
+from server.auth import authenticate, check_update
 from time import time
 
-
 # session cookies will last 24 hours
-SESSION_MAX_AGE = 60*60*24
-
-
-def verify_user(firstName: str, lastName: str, email: str):
-    con, cursor = db_connection()
-
-    try:
-        if not isinstance(firstName, str) or not isinstance(lastName, str) or not isinstance(email, str):
-            return Response({
-            }, mimetype='application/json', status=400)
-        else:
-            cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
-            result = cursor.fetchmany(size=1)
-
-            if len(result) == 1:
-                return Response({
-                }, mimetype='application/json', status=200)
-            else:
-                cursor.execute("INSERT INTO users (firstName, lastName, email, isAdmin) VALUES (%s, %s, %s, 0)",
-                               (firstName, lastName, email))
-                con.commit()
-                return Response({
-                }, mimetype='application/json', status=200)
-    except Exception:
-        return Response({
-        }, mimetype='application/json', status=500)
-    finally:
-        cursor.close()
-        con.close()
+SESSION_MAX_AGE = 60 * 60 * 24
 
 
 def auth_user(email: str):
+    # TODO: Add better route for user logging out, cookies expiring, refreshing
     session_id = request.cookies.get('session')
     if session_id:
         user = session.get('user')
@@ -51,6 +20,8 @@ def auth_user(email: str):
             return Response(json.dumps(user), status=200)
 
     user = authenticate(email, request.form['auth_token'])
+
+    check_update(user['first_name'], user['last_name'], user['email'])
 
     if user is None:
         return Response({}, status=401)
--- server/auth.py
+++ server/auth.py
@@ -3,7 +3,8 @@ from enum import Enum
 from typing import Union, Dict
 from google.oauth2 import id_token
 from google.auth.transport import requests
-from flask import request, session
+from flask import request, session, Response
+from server.utilities import db_connection
 
 
 class AuthStatus(Enum):
@@ -13,6 +14,71 @@ class AuthStatus(Enum):
     Guest = 'Guest',
     User = 'User',
     Admin = 'Admin'
+
+
+def verify_user(firstName: str, lastName: str, email: str):
+    """
+    Check if the user signing in is a user in the database. If not,
+    an entry is created in the user table.
+    :param firstName: First name of the user signing in
+    :param lastName: Last name of the user signing in
+    :param email: Email address of the user signing in
+    :return: Authorization status of the request.
+    """
+    con, cursor = db_connection()
+
+    try:
+        cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+        result = cursor.fetchmany(size=1)
+
+        if len(result) == 1:
+            return Response({
+            }, mimetype='application/json', status=200)
+        else:
+            cursor.execute("INSERT INTO users (firstName, lastName, email, isAdmin) VALUES (%s, %s, %s, 0)",
+                           (firstName, lastName, email))
+            con.commit()
+            return Response({
+            }, mimetype='application/json', status=200)
+    except Exception:
+        return Response({
+        }, mimetype='application/json', status=500)
+    finally:
+        cursor.close()
+        con.close()
+
+
+def check_update(firstName: str, lastName: str, email: str):
+    """
+    Check if the user signing in has updated personal details and update values
+    in the database if need be.
+    NOTE: Only firstName and lastName can be updated
+    :param firstName: First name of the user signing in
+    :param lastName: Last name of the user signing in
+    :param email: Email address of the user signing in
+    :return: Authorization status of the request.
+    """
+    con, cursor = db_connection()
+
+    try:
+        cursor.execute("SELECT firstName, lastName FROM users WHERE email=%s", (email,))
+        result = cursor.fetchmany(size=1)
+
+        if result[0][0] != firstName:
+            cursor.execute("UPDATE users SET firstName=%s WHERE email=%s", (firstName, email,))
+
+        if result[0][1] != lastName:
+            cursor.execute("UPDATE users SET lastName=%s WHERE email=%s", (lastName, email,))
+
+        con.commit()
+        return Response({
+        }, mimetype='application/json', status=200)
+    except Exception:
+        return Response({
+        }, mimetype='application/json', status=500)
+    finally:
+        cursor.close()
+        con.close()
 
 
 def _fetch_token_info(token: str) -> (str, str, str, int):
@@ -39,17 +105,24 @@ def authenticate(email: str, token: str) -> Union[Dict, None]:
     :param token: OAuth token to attempt to validate
     :return: Authorization status of the request.
     """
+    con, cursor = db_connection()
 
     try:
         token_first_name, token_last_name, token_email, token_expiration = _fetch_token_info(token)
         assert email == token_email
 
-        # TODO: Call our server, check if the user is an admin,
-        #  set this to AuthStatus.Admin if they are
-        auth_status = AuthStatus.User
+        verify_user(token_first_name, token_last_name, token_email)
+
+        cursor.execute("SELECT id, isAdmin FROM users WHERE email=%s", (token_email,))
+        result = cursor.fetchmany(size=1)
+
+        if result[0][1] == 1:
+            auth_status = AuthStatus.Admin
+        else:
+            auth_status = AuthStatus.User
 
         user = {
-            'id': 1,  # TODO
+            'id': result[0][0],
             'first_name': token_first_name,
             'last_name': token_last_name,
             'auth_status': auth_status.value[0],
@@ -62,3 +135,7 @@ def authenticate(email: str, token: str) -> Union[Dict, None]:
     except (AssertionError, ValueError):
         print(f'Warning: Invalid login attempt for {email} from {request.remote_addr}.')
         return None
+    finally:
+        cursor.close()
+        con.close()
+
