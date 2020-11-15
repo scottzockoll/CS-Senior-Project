28328ff0beb69d1601018122bd0801b237de19ae
robinsk94 <60240812+robinsk94@users.noreply.github.com>
2020-11-15 15:22:20 -0500

    Added limit variable
    Now limit and offset can be passed to the function to specify a range of rows to be returned

--- server/api/v1/get/get_users.py
+++ server/api/v1/get/get_users.py
@@ -3,8 +3,7 @@ from flask import Flask, Response
 import json
 
 
-def get_users(offset: int):
-
+def get_users(limit: int, offset: int):
     def process_single_tag(tag: str):
         split = tag.split(',')
         return {
@@ -28,7 +27,7 @@ def get_users(offset: int):
         else:
             cursor.execute('''SELECT DISTINCT * FROM FlickPick.master_user_feedback_view '''
                            '''ORDER BY user_id ASC '''
-                           '''LIMIT 10 OFFSET %s;''', (offset,))
+                           '''LIMIT %s OFFSET %s;''', (limit, offset,))
             result = cursor.fetchall()
             if len(result) < 1:
                 return Response({
@@ -47,20 +46,18 @@ def get_users(offset: int):
                         filter_dict["first_name"] = row[1]
                         filter_dict["last_name"] = row[2]
                         filter_dict["is_admin"] = row[4]
-                        filter_dict["movies"] = [{"movie_id": row[5], "title": row[6], "rating": row[7], "tags": process_movie_tags(row[8])}]
+                        filter_dict["movies"] = [
+                            {"movie_id": row[5], "title": row[6], "rating": row[7], "tags": process_movie_tags(row[8])}]
                         users_list.append(filter_dict)
                         filter_dict = dict.fromkeys(["id", "email", "first_name", "last_name", "is_admin", "movies"])
                     # if there is, just append the movie information to the existing dictionary for that user
                     else:
-                        filter_dict["movies"] = {"movie_id": row[5], "title": row[6], "rating": row[7], "tags": process_movie_tags(row[8])}
+                        filter_dict["movies"] = {"movie_id": row[5], "title": row[6], "rating": row[7],
+                                                 "tags": process_movie_tags(row[8])}
                         for dicts in users_list:
                             if dicts["id"] == row[0]:
                                 dicts["movies"].append(filter_dict["movies"])
                         filter_dict = dict.fromkeys(["id", "email", "first_name", "last_name", "is_admin", "movies"])
-
-                print(users_list)
-
-
 
                 return Response(json.dumps(users_list), mimetype='application/json', status=200)
     except Exception:
