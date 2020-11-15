101c796b64c63f35e2f2b6e32251439b26d74e23
robinsk94 <60240812+robinsk94@users.noreply.github.com>
2020-11-15 15:51:35 -0500

    Added pydocs for functions + var checks
    Wrote out the pydocs for the function, and helpers to describe what their purpose is as well as adding code to check that limit is an int.

--- server/api/v1/get/get_users.py
+++ server/api/v1/get/get_users.py
@@ -4,7 +4,28 @@ import json
 
 
 def get_users(limit: int, offset: int):
+    """
+    Retrieves multiple rows from the master_user_feedback_view in the database
+    given an int for row limit, and an int for the offset of where the SELECT
+    statement starts.
+    :param int limit: The limit which restricts how many rows are returned
+    :param int offset: This specifies the offset of the first row to return.
+    :return: A JSON object containing a list of dictionaries containing
+    user information. The 'movie' key in each dictionary is also a list
+    of dictionaries, with the 'tag' key for those also being a list of
+    dictionaries.
+    """
+
     def process_single_tag(tag: str):
+        """
+        This is called by process_movie_tags to create
+        a dictionary for each tag. It turns the list indice
+        from the prior function into a dictionary wherein the
+        id, user's rating, and the name of the tag are contained.
+        :param tag: The string from the list created by process_movie_tags
+        :return: A dictionary with key, value pairs for
+        'tag_id', 'rating', and 'name'
+        """
         split = tag.split(',')
         return {
             'tag_id': split[0],
@@ -13,6 +34,16 @@ def get_users(limit: int, offset: int):
         }
 
     def process_movie_tags(movie: str):
+        """
+        Receives a string with all of a movies tags and their info.
+        It splits at the ';' character to create a list where each
+        indice has a single tag's info. It is further processed by
+        process_single_tag.
+        :param movie: This is a string of all the tags and their info
+         for a movie.
+        :return: A list of dictionaries with key, value pairs for
+        'tag_id', 'rating', and 'name'.
+        """
         tag_data = movie.split(';')
         return [process_single_tag(tag) for tag in tag_data]
 
@@ -22,6 +53,9 @@ def get_users(limit: int, offset: int):
             return Response({
             }, mimetype='application/json', status=403)
         if not isinstance(offset, int):  # checks if id is an integer
+            return Response({
+            }, mimetype='application/json', status=400)
+        if not isinstance(limit, int):  # checks if id is an integer
             return Response({
             }, mimetype='application/json', status=400)
         else:
