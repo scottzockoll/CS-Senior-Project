81b7be85dc21f4e37d9678bc823713ee63f5a6a4
Andy <andrewcuccinello@gmail.com>
2020-11-20 11:27:34 -0500

    Organize movie/index.ts

--- client/src/store/movie/index.ts
+++ client/src/store/movie/index.ts
@@ -1,6 +1,21 @@
 import { ApiRequest } from '../api';
 import { Tag } from '../tag';
 
+/**
+ * A single movie.
+ */
+export interface Movie {
+    id: number;
+    title: string;
+    genres: Record<number, string>;
+    rating: number;
+    tags: Record<number, Tag>;
+}
+
+/**
+ * Movie Search
+ */
+
 export const REQUEST_MOVIE_SEARCH_STARTED = 'REQUEST_MOVIE_SEARCH_STARTED';
 export const RECEIVE_MOVIE_SEARCH_SUCCESS = 'RECEIVE_MOVIE_SEARCH_SUCCESS';
 export const RECEIVE_MOVIE_SEARCH_FAILURE = 'RECEIVE_MOVIE_SEARCH_FAILURE';
@@ -13,37 +28,6 @@ export type MOVIE_SEARCH_TYPES =
     | REQUEST_MOVIE_SEARCH_STARTED
     | RECEIVE_MOVIE_SEARCH_SUCCESS
     | RECEIVE_MOVIE_SEARCH_FAILURE;
-
-export const UPDATE_MOVIE_RATING_STARTED = 'UPDATE_MOVIE_RATING_STARTED';
-export const UPDATE_MOVIE_RATING_SUCCESS = 'UPDATE_MOVIE_RATING_SUCCESS';
-export const UPDATE_MOVIE_RATING_FAILURE = 'UPDATE_MOVIE_RATING_FAILURE';
-
-export type UPDATE_MOVIE_RATING_STARTED = typeof UPDATE_MOVIE_RATING_STARTED;
-export type UPDATE_MOVIE_RATING_SUCCESS = typeof UPDATE_MOVIE_RATING_SUCCESS;
-export type UPDATE_MOVIE_RATING_FAILURE = typeof UPDATE_MOVIE_RATING_FAILURE;
-
-export const DELETE_MOVIES_STARTED = 'DELETE_MOVIES_STARTED';
-export const DELETE_MOVIES_SUCCESS = 'DELETE_MOVIES_SUCCESS';
-export const DELETE_MOVIES_FAILURE = 'DELETE_MOVIES_FAILURE';
-
-export type DELETE_MOVIES_STARTED = typeof DELETE_MOVIES_STARTED;
-export type DELETE_MOVIES_SUCCESS = typeof DELETE_MOVIES_SUCCESS;
-export type DELETE_MOVIES_FAILURE = typeof DELETE_MOVIES_FAILURE;
-
-export type MovieUpdateEntitiesTypes =
-    | UPDATE_MOVIE_RATING_STARTED
-    | UPDATE_MOVIE_RATING_SUCCESS
-    | UPDATE_MOVIE_RATING_FAILURE;
-
-export interface Movie {
-    id: number;
-    title: string;
-    genres: Record<number, string>;
-    rating: number;
-    tags: Record<number, Tag>;
-}
-
-export type MovieDeleteEntitiesTypes = DELETE_MOVIES_STARTED | DELETE_MOVIES_SUCCESS | DELETE_MOVIES_FAILURE;
 
 export interface SearchMovieStarted extends ApiRequest {
     type: REQUEST_MOVIE_SEARCH_STARTED;
@@ -64,7 +48,17 @@ export interface SearchMovieFailure extends ApiRequest {
 
 export type SearchMovieActions = SearchMovieStarted | SearchMovieSuccess | SearchMovieFailure;
 
-export const GET_MOVIE = 'GET_MOVIE';
+/**
+ * Update Movie Rating
+ */
+
+export const UPDATE_MOVIE_RATING_STARTED = 'UPDATE_MOVIE_RATING_STARTED';
+export const UPDATE_MOVIE_RATING_SUCCESS = 'UPDATE_MOVIE_RATING_SUCCESS';
+export const UPDATE_MOVIE_RATING_FAILURE = 'UPDATE_MOVIE_RATING_FAILURE';
+
+export type UPDATE_MOVIE_RATING_STARTED = typeof UPDATE_MOVIE_RATING_STARTED;
+export type UPDATE_MOVIE_RATING_SUCCESS = typeof UPDATE_MOVIE_RATING_SUCCESS;
+export type UPDATE_MOVIE_RATING_FAILURE = typeof UPDATE_MOVIE_RATING_FAILURE;
 
 export interface UpdateMovieRatingStarted extends ApiRequest {
     type: UPDATE_MOVIE_RATING_STARTED;
@@ -79,6 +73,27 @@ export interface UpdateMovieRatingSuccess {
 export interface UpdateMovieRatingFailure {
     type: UPDATE_MOVIE_RATING_FAILURE;
 }
+
+export type MovieEntitiesActions = UpdateMovieRatingStarted | UpdateMovieRatingSuccess | UpdateMovieRatingFailure;
+
+/**
+ * Delete Movie
+ */
+
+export const DELETE_MOVIES_STARTED = 'DELETE_MOVIES_STARTED';
+export const DELETE_MOVIES_SUCCESS = 'DELETE_MOVIES_SUCCESS';
+export const DELETE_MOVIES_FAILURE = 'DELETE_MOVIES_FAILURE';
+
+export type DELETE_MOVIES_STARTED = typeof DELETE_MOVIES_STARTED;
+export type DELETE_MOVIES_SUCCESS = typeof DELETE_MOVIES_SUCCESS;
+export type DELETE_MOVIES_FAILURE = typeof DELETE_MOVIES_FAILURE;
+
+export type MovieUpdateEntitiesTypes =
+    | UPDATE_MOVIE_RATING_STARTED
+    | UPDATE_MOVIE_RATING_SUCCESS
+    | UPDATE_MOVIE_RATING_FAILURE;
+
+export type MovieDeleteEntitiesTypes = DELETE_MOVIES_STARTED | DELETE_MOVIES_SUCCESS | DELETE_MOVIES_FAILURE;
 
 export interface DeleteMoviesStarted extends ApiRequest {
     type: DELETE_MOVIES_STARTED;
@@ -95,8 +110,4 @@ export interface DeleteMoviesFailure {
     id: number;
 }
 
-/**
- * Any movie entities action, that is a movie entities Request {Start, Success, Failure}
- */
-export type MovieEntitiesActions = UpdateMovieRatingStarted | UpdateMovieRatingSuccess | UpdateMovieRatingFailure;
 export type MovieDeleteActions = DeleteMoviesStarted | DeleteMoviesSuccess | DeleteMoviesFailure;
