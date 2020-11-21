99ec0bef98292496137fba4098f1db4fd6dac18d
Andy <andrewcuccinello@gmail.com>
2020-11-21 18:30:40 -0500

    Fix reducer

--- client/src/routes/common/SearchField.tsx
+++ client/src/routes/common/SearchField.tsx
@@ -1,4 +1,4 @@
-import { Box, TextInput } from 'grommet';
+import { Box, Drop, DropType, TextInput } from 'grommet';
 import React from 'react';
 import { connect } from 'react-redux';
 import en from '../../en.json';
@@ -6,7 +6,7 @@ import { AppDispatch, RootState } from '../../store';
 import { searchMovie } from '../../store/movie/actions';
 
 const mapStateToProps = (state: RootState) => ({
-    // to do
+    movies: state.movieSearchResults.ids.map((id) => state.movieSearchResults.entities[id]),
 });
 
 const mapDispatchToProps = (dispatch: AppDispatch) => ({
@@ -15,17 +15,44 @@ const mapDispatchToProps = (dispatch: AppDispatch) => ({
 
 type SearchFieldProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
-const UnconnectedSearchField: React.FC<SearchFieldProps> = ({ movieSearch }) => {
-    return (
-        <TextInput
-            placeholder={en.UI_LABELS.movieSearch}
-            // dropProps={}
-            onInput={(event: React.SyntheticEvent) => {
-                const input = event.target as HTMLInputElement;
-                movieSearch(input.value);
-            }}
-        />
-    );
-};
+class UnconnectedSearchField extends React.Component<SearchFieldProps> {
+    protected dropRef: React.RefObject<DropType>;
+    protected inputRef: React.RefObject<typeof TextInput>;
+
+    constructor(props: Readonly<SearchFieldProps>) {
+        super(props);
+
+        this.dropRef = React.createRef();
+        this.inputRef = React.createRef();
+    }
+
+    render() {
+        console.warn(this.props.movies);
+        return (
+            <Box>
+                <TextInput
+                    placeholder={en.UI_LABELS.movieSearch}
+                    dropTarget={this.dropRef}
+                    dropProps={{
+                        align: {
+                            top: 'bottom',
+                        },
+                    }}
+                    onInput={(event: React.SyntheticEvent) => {
+                        const input = event.target as HTMLInputElement;
+                        this.props.movieSearch(input.value);
+                    }}
+                />
+                <Drop ref={'dropRef'} target={this.inputRef}>
+                    <ul>
+                        {this.props.movies.map((movie) => (
+                            <li key={movie.id}>{movie.title}</li>
+                        ))}
+                    </ul>
+                </Drop>
+            </Box>
+        );
+    }
+}
 
 export const SearchField = connect(mapStateToProps, mapDispatchToProps)(UnconnectedSearchField);
