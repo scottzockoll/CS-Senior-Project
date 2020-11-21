7869b7b2bc8e82be57722d308b3ad6c43abae610
Andy <andrewcuccinello@gmail.com>
2020-11-21 16:00:23 -0500

    Cleanup InitialSurveyModal.tsx

--- client/src/routes/home/InitialSurveyModal.tsx
+++ client/src/routes/home/InitialSurveyModal.tsx
@@ -1,10 +1,14 @@
-import { Box, Button, Grid, Heading } from 'grommet';
+import { Box, Button, Heading } from 'grommet';
 import React from 'react';
 import { toggleInitialSurveyModal } from '../../store/home/actions';
 import { AppDispatch, RootState } from '../../store';
 import { connect } from 'react-redux';
 import { SearchField } from '../common/SearchField';
 import StarRating from '../common/star/StarRating';
+
+interface PassedProps {
+    numMovies: number;
+}
 
 const mapStateToProps = (state: RootState) => ({
     initialSurveyVisible: state.initialSurveyVisible,
@@ -14,10 +18,35 @@ const mapDispatchToProps = (dispatch: AppDispatch) => ({
     toggleInitialSurveyModal: (isVisible: boolean) => dispatch(toggleInitialSurveyModal(isVisible)),
 });
 
-type InitialSurveyProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+type InitialSurveyProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps> & PassedProps;
 
-const InitialSurveyComponent: React.FC<InitialSurveyProps> = ({ initialSurveyVisible, toggleInitialSurveyModal }) => {
-    const numberOfChildren: number[] = [1, 2, 3, 4, 5];
+/**
+ * Construct a list of movie search fields
+ * @param props
+ * @constructor
+ */
+const MovieSearchList = (props: { count: number }) => {
+    let fields: JSX.Element[] = [];
+    for (let i = 0; i < props.count; i++) {
+        fields.push(
+            <Box direction={'row'} key={i}>
+                <Box flex={'grow'}>
+                    <SearchField />
+                </Box>
+                <Box pad={{ horizontal: 'xlarge' }}>
+                    <StarRating currentRating={0} numberOfStars={5} size={'medium'} />
+                </Box>
+            </Box>
+        );
+    }
+    return <React.Fragment>{fields}</React.Fragment>;
+};
+
+const InitialSurveyComponent: React.FC<InitialSurveyProps> = ({
+    initialSurveyVisible,
+    toggleInitialSurveyModal,
+    numMovies,
+}) => {
     return (
         <Box>
             {initialSurveyVisible && (
@@ -25,18 +54,10 @@ const InitialSurveyComponent: React.FC<InitialSurveyProps> = ({ initialSurveyVis
                     <Box margin={{ left: 'auto', right: 'auto' }} direction="row">
                         <Heading>Initial Survey</Heading>
                     </Box>
-                    {numberOfChildren.map(() => {
-                        return (
-                            <Box direction={'row'}>
-                                <Box flex={'grow'}>
-                                    <SearchField />
-                                </Box>
-                                <Box pad={{ horizontal: 'xlarge' }}>
-                                    <StarRating currentRating={0} numberOfStars={5} size={'medium'} />
-                                </Box>
-                            </Box>
-                        );
-                    })}
+
+                    {/*<SurveyFields n={numMovies} />*/}
+                    <MovieSearchList count={numMovies} />
+
                     <Box width="35%" margin={{ top: 'medium', left: 'auto', right: 'auto' }}>
                         <Box>
                             <Button
