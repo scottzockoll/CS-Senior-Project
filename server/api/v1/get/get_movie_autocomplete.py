9df9613887ee8e8d85d18bb2b6039575be969de1
Andy <andrewcuccinello@gmail.com>
2020-11-21 17:56:18 -0500

    Update get movie autocomplete

--- server/api/v1/get/get_movie_autocomplete.py
+++ server/api/v1/get/get_movie_autocomplete.py
@@ -1,3 +1,5 @@
+import json
+
 from server.utilities import db_connection, is_user
 from Levenshtein import distance
 from flask import Response
@@ -13,8 +15,7 @@ def get_movie_autocomplete(name: str):
 
     try:
         if not is_user():
-            return Response({
-            }, mimetype='application/json', status=403)
+            return Response({}, mimetype='application/json', status=403)
         else:
             cursor.execute(f"SELECT id, name FROM movies WHERE name LIKE '{name}__%'")
 
@@ -23,12 +24,13 @@ def get_movie_autocomplete(name: str):
             if len(result) == 0:
                 return []
             else:
-                distances = [(distance(a[1], name), a[1]) for a in result]
-                titles = list(map(lambda movie: {'id': movie[0], 'title': movie[1]}, sorted(distances)))[:10]
+                distances = [(distance(a[1], name), a[0], a[1]) for a in result]
+                titles = list(map(lambda movie: {'id': movie[1], 'title': movie[2]}, sorted(distances)))[:10]
 
-                return Response({            # TODO: Update to return complete object (similar to get_movie)
+                # TODO: Update to return complete object (similar to get_movie)
+                return Response(json.dumps({
                     "movies": titles
-                }, mimetype='application/json', status=200)
+                }), mimetype='application/json', status=200)
     except Exception:
         return Response({
         }, mimetype='application/json', status=500)
