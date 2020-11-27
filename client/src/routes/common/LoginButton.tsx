d227d5cbb51d14a0182da1634436922764b4cde2
Andy <andrewcuccinello@gmail.com>
2020-11-27 13:22:14 -0500

    Login button displays loading status

--- client/src/routes/common/LoginButton.tsx
+++ client/src/routes/common/LoginButton.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { AppDispatch, RootState } from '../../store';
-import { requestSingleUser, requestUsers, updateToken, userLogin } from '../../store/user/actions';
+import { requestSingleUser, updateToken, userLogin } from '../../store/user/actions';
 import { User } from '../../store/user';
 import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login';
 import { API_ROOT } from '../../store/api';
 
+const mapStateToProps = (state: RootState) => ({
+    loading: state.users.isFetching,
+});
 type LoginButtonProps = ReturnType<typeof mapStateToProps> &
     ReturnType<typeof mapDispatchToProps> & {
         clientId: string;
+        loading: boolean;
     };
 
 class LoginButton extends React.Component<LoginButtonProps> {
@@ -16,17 +20,16 @@ class LoginButton extends React.Component<LoginButtonProps> {
         return (
             <GoogleLogin
                 clientId={this.props.clientId}
-                buttonText="Login with Google"
+                buttonText={this.props.loading ? 'Logging you in...' : 'Login w/ Google'}
                 onSuccess={this.props.login}
                 cookiePolicy={'single_host_origin'}
-                // TODO: should not be always true
+                // TODO: should not be always true?
                 isSignedIn={true}
             />
         );
     }
 }
 
-const mapStateToProps = (state: RootState) => ({});
 const mapDispatchToProps = (dispatch: AppDispatch) => ({
     login: async (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
         const isOnlineLogin = (r: GoogleLoginResponse | GoogleLoginResponseOffline): r is GoogleLoginResponse => {
@@ -41,8 +44,6 @@ const mapDispatchToProps = (dispatch: AppDispatch) => ({
             const auth = async (email: string, token: string) => {
                 let form_data = new FormData();
                 form_data.append('auth_token', token);
-
-                console.warn(`CALLING: "${API_ROOT}auth/${email}"`);
 
                 const userLoginPost = await fetch(`${API_ROOT}auth/${email}`, {
                     method: 'POST',
