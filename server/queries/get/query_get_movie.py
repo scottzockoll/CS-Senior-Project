4b7c5247b45aad81c38b4a60ce44661146907094
Justus Fee <59373880+justusRSU@users.noreply.github.com>
2020-12-02 17:55:03 -0500

    Last SQL extractions

--- server/queries/get/query_get_movie.py
+++ server/queries/get/query_get_movie.py
@@ -13,28 +13,34 @@ def query_get_movie(id: Union[int, str]):
         cursor.execute(
             '''SELECT distinct movies.id, movies.name, group_concat(DISTINCT tags.name ORDER BY tags.name ASC SEPARATOR ',') as 'Tags', '''
             '''group_concat(DISTINCT genre.genre ORDER BY genre.genre ASC SEPARATOR ',') as 'Genres' FROM FlickPick.movies '''
-            '''JOIN tags '''
-            '''ON tags.movie_id = movies.id '''
-            '''JOIN genre '''
-            '''on genre.movie_id = movies.id '''
+            '''LEFT JOIN tags ON tags.movie_id = movies.id '''
+            '''LEFT JOIN genre ON genre.movie_id = movies.id '''
             '''WHERE movies.id = %s;''', (id,))
         result = cursor.fetchall()
 
         if (len(result)) == 0:
             return None
+        else:
+            movie_id = result[0][0]
+            movie_name = result[0][1]
+            if result[0][2] is None:
+                tags = [[]]
+            else:
+                tags = [tags[2].split(',') for tags in result]
 
-        movie_id = result[0][0]
-        movie_name = result[0][1]
-        tags = [tags[2].split(',') for tags in result]
-        genres = [genres[3].split(',') for genres in result]
-        data = {
-            "movie_id": movie_id,
-            "movie_name": movie_name,
-            "tags": tags,
-            "genres": genres
-        }
+            if result[0][3] is None:
+                genres = [[]]
+            else:
+                genres = [genres[3].split(',') for genres in result]
+                data = {
+                    "movie_id": movie_id,
+                    "movie_name": movie_name,
+                    "tags": tags[0],
+                    "genres": genres[0]
+                }
+            
+                return data
 
-        return data
     finally:
         con.close()
         cursor.close()
