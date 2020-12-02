779db854d498181b895bf711e74ca45a6b87dead
robinsk94 <60240812+robinsk94@users.noreply.github.com>
2020-12-02 13:08:03 -0500

    Extract SQL Logic from and update get_users.py and get_user.py (#131)
    * Extracted SQL logic from endpoint
    
    * Extracted SQL logic from endpoint
    
    * Created file to contain SQL logic for query
    
    * Created a file with extracted SQL Logic
    
    Extracted SQL logic for 'get_users.py' and addressed weird results

--- server/api/v1/get/get_users.py
+++ server/api/v1/get/get_users.py
@@ -1,5 +1,6 @@
+from server.queries.get.query_get_users import query_get_users
 import server.utilities
-from flask import Flask, Response
+from flask import Response
 import json
 
 
@@ -16,53 +17,19 @@ def get_users(limit: int, offset: int):
     dictionaries.
     """
 
-    con, cursor = server.utilities.db_connection()
+
     try:
         if not server.utilities.is_user():
-            return Response({
-            }, mimetype='application/json', status=403)
+            return Response({}, mimetype='application/json', status=403)
+
         if not isinstance(limit, int) or not isinstance(offset, int):  # checks if id is an integer
-            return Response({
-            }, mimetype='application/json', status=400)
+            return Response({}, mimetype='application/json', status=400)
+
+        result = query_get_users(limit, offset)
+        if not result:
+            return Response({}, mimetype='application/json', status=404)
         else:
-            cursor.execute('''SELECT DISTINCT * FROM FlickPick.master_user_feedback_view '''
-                           '''ORDER BY user_id ASC '''
-                           '''LIMIT %s OFFSET %s;''', (limit, offset,))
-            result = cursor.fetchall()
-            if len(result) < 1:
-                return Response({
-                }, mimetype='application/json', status=404)
-            else:
+            return Response(json.dumps(result), mimetype='application/json', status=200)
 
-                filter_dict = dict.fromkeys(["id", "email", "first_name", "last_name", "is_admin", "movies"])
-
-                users_list = []
-
-                for row in result:
-                    # if there is not an instance of the user, create a dictionary in the list for them
-                    if not any(d['id'] == row[0] for d in users_list):
-                        filter_dict["id"] = row[0]
-                        filter_dict["email"] = row[3]
-                        filter_dict["first_name"] = row[1]
-                        filter_dict["last_name"] = row[2]
-                        filter_dict["is_admin"] = row[4]
-                        filter_dict["movies"] = [
-                            {"movie_id": row[6], "title": row[5], "rating": row[7], "tags": server.utilities.process_movie_tags(row[8])}]
-                        users_list.append(filter_dict)
-                        filter_dict = dict.fromkeys(["id", "email", "first_name", "last_name", "is_admin", "movies"])
-                    # if there is, just append the movie information to the existing dictionary for that user
-                    else:
-                        filter_dict["movies"] = {"movie_id": row[6], "title": row[5], "rating": row[7],
-                                                 "tags": server.utilities.process_movie_tags(row[8])}
-                        for dicts in users_list:
-                            if dicts["id"] == row[0]:
-                                dicts["movies"].append(filter_dict["movies"])
-                        filter_dict = dict.fromkeys(["id", "email", "first_name", "last_name", "is_admin", "movies"])
-
-                return Response(json.dumps(users_list), mimetype='application/json', status=200)
     except Exception:
-        return Response({
-        }, mimetype='application/json', status=500)
-    finally:
-        cursor.close()
-        con.close()
+        return Response({}, mimetype='application/json', status=500)
