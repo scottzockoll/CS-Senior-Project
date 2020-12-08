a00d414082f5a5191701238f1e3f6cf4cfc6d399
thefinancialfix <thefinancialfix@users.noreply.github.com>
2020-12-07 21:37:48 -0500

    add generic carousel

--- client/src/routes/home/MovieCarousel.tsx
+++ client/src/routes/home/MovieCarousel.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import { Box, Carousel } from 'grommet';
 import { toggleMovieModal } from '../../store/home/actions';
-
+import { title } from 'process';
 class MovieCarousel extends React.Component {
     render() {
+        const mockMovieTitles = [
+            'Ant-Man',
+            '42 Hours',
+            'The Wolf of Wall St.',
+            'Spider-Man: Homecoming',
+            'Saving Private Ryan',
+            'Elf',
+            'Christmas Vacation',
+            'What Happened to Moday?',
+            'Wedding Crashers',
+            'Blackhawk Down',
+            'Beverly Hills Cop',
+            'Guardians of the Galaxy',
+            'Scream',
+            'Halloween',
+            'Toy Story',
+            'Cars',
+            'Captain America: The First Avenger',
+            'Avatar',
+            'Deadpool',
+            'The Sound of Music',
+            'Freaky Friday',
+            'Doctor Strange',
+            'Star Wars: The Last Jedi',
+            'Zootopia',
+            'Finding Nemo',
+        ];
         return (
             <Box>
                 <Carousel
@@ -12,22 +39,15 @@ class MovieCarousel extends React.Component {
                     }}
                     fill
                     play={5000}
+                    controls={'arrows'}
                 >
-                    <Box margin={{ bottom: '12px' }}>
-                        <h1>Title 1</h1>
-                    </Box>
-                    <Box margin={{ bottom: '12px' }}>
-                        <h1>Title 2</h1>
-                    </Box>
-                    <Box margin={{ bottom: '12px' }}>
-                        <h1>Title 3</h1>
-                    </Box>
-                    <Box margin={{ bottom: '12px' }}>
-                        <h1>Title 4</h1>
-                    </Box>
-                    <Box margin={{ bottom: '12px' }}>
-                        <h1>Title 5</h1>
-                    </Box>
+                    {mockMovieTitles.map(function (title, index) {
+                        return (
+                            <Box margin={{ bottom: '12px' }}>
+                                <h1 key={index}>{title}</h1>
+                            </Box>
+                        );
+                    })}
                 </Carousel>
             </Box>
         );
