b1236cc24198fd52f6f571de54e20238efedfa91
thefinancialfix <thefinancialfix@users.noreply.github.com>
2020-12-10 22:50:44 -0500

    update Cypress functional test suite for UI changes

--- tests/src/integration/movieSurvey.spec.ts
+++ tests/src/integration/movieSurvey.spec.ts
@@ -1,12 +1,30 @@
-import { fillInitialSurvey, navigateToHomepage, openInitialSurveyModal } from "../models/homepage/homepage";
+import {
+    closeInitialSurveyModal,
+    navigateToHomepage,
+    openInitialSurveyModal,
+    selectRatings,
+} from '../models/homepage/homepage';
 
 describe('movie survey', () => {
     before(() => navigateToHomepage());
 
-    const movieTitles: string[] = ['Wedding Crashers', 'Toy Story', 'The Social Network', 'Whiplash', 'What Happened to Monday?'];
+    const movieTitles: string[] = [
+        'Wedding Crashers',
+        'Toy Story',
+        'The Social Network',
+        'Whiplash',
+        'What Happened to Monday?',
+    ];
 
     it('can open initial survey modal', () => {
         openInitialSurveyModal();
-        fillInitialSurvey(movieTitles);
+    });
+
+    it('can select ratings', () => {
+        selectRatings();
+    });
+
+    it('can open initial survey modal', () => {
+        closeInitialSurveyModal();
     });
 });
